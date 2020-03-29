f2c1008bb854d522d5b42eb71d6d5cf3eeb188fe
Osvaldo Kalvaitir Filho <osvaldokalvaitir@outlook.com>
2020-03-29 12:22:30 -0300

    Testando formulário

--- src/components/TechList/index.js
+++ src/components/TechList/index.js
@@ -2,17 +2,23 @@ import React, { useState } from 'react';
 
 export default function TechList() {
   const [techs, setTechs] = useState([]);
+  const [newTech, setNewTech] = useState('');
 
   function handleAddTech() {
-    setTechs([...techs, 'Node.js']);
+    setTechs([...techs, newTech]);
+    setNewTech('');
   }
 
   return (
-    <div>
+    <form data-testid="tech-form" onSubmit={handleAddTech}>
       <ul data-testid="tech-list">
         {techs.map(tech => <li key={tech}>{tech}</li>)}
       </ul>
+
+      <label htmlFor="tech">Tech</label>
+      <input id="tech" value={newTech} onChange={e => setNewTech(e.target.value)} />
+
       <button onClick={handleAddTech}>Adicionar</button>
-    </div>
+    </form>
   );
 }
