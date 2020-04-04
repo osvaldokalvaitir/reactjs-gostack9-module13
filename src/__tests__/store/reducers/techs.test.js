ab1508d668b4e6837a2badc99aabf95921e1c635
Osvaldo Kalvaitir Filho <osvaldokalvaitir@outlook.com>
2020-04-04 14:18:48 -0300

    Coverage report

--- src/__tests__/store/reducers/techs.test.js
+++ src/__tests__/store/reducers/techs.test.js
@@ -2,6 +2,12 @@ import reducer, { INITIAL_STATE } from '~/store/modules/techs/reducer';
 import * as Techs from '~/store/modules/techs/actions';
 
 describe('Techs reducer', () => {
+  it('DEFAULT', () => {
+    const state = reducer(undefined, {});
+
+    expect(state).toStrictEqual(INITIAL_STATE);
+  });
+
   it('ADD_TECH', () => {
     const state = reducer(INITIAL_STATE, Techs.addTech('Node.js'));
 
